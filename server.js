1c62150e938fe9b73b0234b9e36910cef7a40aec
czulueta <https://github.com/czulueta/weed.git>
2021-03-29 23:18:16 -0700

    setup basic server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+
+app.use(express.json())
+app.use(morgan("dev"))
+
+mongoose.connect("mongodb://localhost:/27017/betTrackingApp",
+    {   useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    () => console.log("connected to the database"))
+
+app.use("/wagers", require("./routes/wagerRouter.js"))
+
